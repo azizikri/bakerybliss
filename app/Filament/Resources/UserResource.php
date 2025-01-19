5fd359f7ec98eeceb2eaecb5b09c35381811e82b
azizikri <azizikri@gmail.com>
2025-01-19 22:29:05 +0700

    feat: enhance User resource navigation with grouping and sorting

--- app/Filament/Resources/UserResource.php
+++ app/Filament/Resources/UserResource.php
@@ -19,6 +19,9 @@ class UserResource extends Resource
     protected static ?string $model = User::class;
 
     protected static ?string $navigationIcon = 'heroicon-o-user';
+    protected static ?string $navigationGroup = 'User Management';
+    protected static ?int $navigationSort = 2;
+    protected static bool $shouldRegisterNavigation = true;
 
     public static function form(Form $form): Form
     {
