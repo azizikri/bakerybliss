d814ded73bd0b98b4e22590533c804d3d2ed3f2d
azizikri <azizikri@gmail.com>
2025-01-24 14:28:40 +0700

    feat: add deployment script and update CI workflow for automated deployment

---
+++ build.sh
@@ -0,0 +1,23 @@
+#!/bin/bash
+
+echo "Running deploy script"
+
+echo "[1/6] Pulling from GitHub"
+git pull origin
+
+echo "[2/6] Creating database if one isn't found"
+touch database/database.sqlite
+
+echo "[3/6] Installing packages using composer"
+composer install
+
+echo "[4/6] Installing packages using npm"
+npm install
+
+echo "[5/10] Installing packages using npm"
+npm run build
+
+echo "[6/6] Migrating database"
+php artisan migrate --force
+
+echo "The app has been built and deployed!"
