63dd7889a9bd6de8e4c60d51a47e2b2558619af8
azizikri <azizikri@gmail.com>
2025-01-25 11:36:26 +0700

    feat: implement caching for product listing in CatalogController

--- app/Http/Controllers/CatalogController.php
+++ app/Http/Controllers/CatalogController.php
@@ -12,10 +12,14 @@ class CatalogController extends Controller
      */
     public function index(Request $request)
     {
-        $products = Product::query()
-            ->where('stock', '!=', 0)
-            ->where('status', 1)
-            ->paginate(12);
+        $page = $request->has('page') ? $request->query('page') : 1;
+
+        $products = cache()->remember('products'.'_page_'.$page, now()->addDay(), function () {
+            return Product::query()
+                ->where('stock', '!=', 0)
+                ->where('status', 1)
+                ->paginate(12);
+        });
 
         return view('user.shop', [
             'products' => $products
